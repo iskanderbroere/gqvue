65ef6549c64a19d80406deb8c5bc85db7b8ec9b9
Iskander Broere <iskanderbroere@gmail.com>
2018-03-06 12:22:52 +0100

    exception

--- apollo/client-configs/default.js
+++ apollo/client-configs/default.js
@@ -2,20 +2,23 @@ import { ApolloLink } from "apollo-link"
 import { HttpLink } from "apollo-link-http"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import cookie from "cookie"
+import jscookies from "js-cookie"
 
-export default ctx => {
+export default ({ req }) => {
   const httpLink = new HttpLink({ uri: "http://localhost:4000" })
   const middlewareLink = new ApolloLink((operation, forward) => {
     const cookies = () => {
       if (process.server) {
-        return cookie.parse(ctx.req.headers.cookie || "")
-      } else if (process.client && ctx.store.state.user) {
-        return { "bonas-access-token": ctx.store.state.user.token }
+        return cookie.parse(req.headers.cookie || "")
+      } else if (process.client) {
+        return {
+          "bonas-access-token": jscookies.get("bonas-access-token") || ""
+        }
       }
       return {}
     }
     if (cookies().hasOwnProperty("bonas-access-token")) {
-      const token = `Bearer ${cookies()["bonas-access-token"]}`
+      const token = "Bearer " + cookies()["bonas-access-token"]
       operation.setContext({
         headers: {
           authorization: token
